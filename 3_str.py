6ea08a7ae5b93176be9b895d9f075f59e1f0f18f
Татьяна <119119137+SuperBobaBiba@users.noreply.github.com>
2022-12-02 21:51:59 +0300

    Add files via upload

---
+++ 3_str.py
@@ -0,0 +1,2 @@
+str = input('Введите строку:\n')
+print(str[len(str)-1], str[len(str)-2], sep="")
---
+++ 4_krug.py
@@ -0,0 +1,3 @@
+import math
+r=float(input('Введите радиус круга:\nr='))
+print ('Площадь круга=', r**2*(math.pi))
